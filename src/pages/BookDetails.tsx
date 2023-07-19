9d13b0d4b76f0b75beeb30ecf1b4104c471620b8
sazzad <sazzadkarim01@gmail.com>
2023-07-19 20:16:24 +0600

    Book details page commited

---
+++ src/pages/BookDetails.tsx
@@ -0,0 +1,40 @@
+import {
+  useDeleteSingleBookMutation,
+  useGetSingleBookQuery,
+} from '@/redux/api/apiSlice';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+const BookDetails = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { data } = useGetSingleBookQuery(id);
+
+  const [deleteBook, _options] = useDeleteSingleBookMutation();
+
+  const handleDelete = () => {
+    deleteBook(id);
+    navigate('/books');
+  };
+
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl mx-auto mt-10">
+      <div className="card-body">
+        <h2 className="card-title">{data?.data?.Title}</h2>
+        <p>{data?.data?.Genre}</p>
+        <p>{data?.data?.Author}</p>
+        <p>{data?.data?.PublishedDate}</p>
+        <div className="card-actions justify-between">
+          <Link to={`/books/${id}`} className="btn bg-green-300">
+            Edit
+          </Link>
+          <button className="btn bg-red-400" onClick={handleDelete}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookDetails;
