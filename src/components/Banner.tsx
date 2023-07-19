fe26671a03d310a7b4f85a363d2d72675c657e23
sazzad <sazzadkarim01@gmail.com>
2023-07-19 20:05:56 +0600

    Banner commited successfully

---
+++ src/components/Banner.tsx
@@ -0,0 +1,16 @@
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div>
+        <h2>
+          Read The Books & _ <br />
+        </h2>
+      </div>
+      <div>
+        <h2>Improve Your Productivity </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
+import Banner from '@/components/Banner';
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
