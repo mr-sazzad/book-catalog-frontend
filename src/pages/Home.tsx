de8c611f411e301617523d5717fd0a1267a201b4
sazzad <sazzadkarim01@gmail.com>
2023-07-19 20:35:11 +0600

    Home page modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Banner from '@/components/Banner';
+import Footer from '@/components/Footer';
 import NewBooks from '@/components/RecentlyAdded';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <div>
       <Banner />
       <NewBooks />
+      <Footer />
     </div>
   );
 };
