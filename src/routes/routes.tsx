a6cf252d0aeba96b5051f4c454581137945985cf
sazzad <sazzadkarim01@gmail.com>
2023-07-19 20:04:10 +0600

    Routes setup success to main.tsx

---
+++ src/routes/routes.tsx
@@ -0,0 +1,18 @@
+import App from '@/App';
+import Home from '@/pages/Home';
+import { createBrowserRouter } from 'react-router-dom';
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default routes;
