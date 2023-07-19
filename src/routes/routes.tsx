0b6ddb79586d159f7727c804be39621645f7b6d0
sazzad <sazzadkarim01@gmail.com>
2023-07-19 20:21:55 +0600

    routes file commited

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,13 @@
 import App from '@/App';
+import EditBook from '@/components/EditBook';
+import BookDetails from '@/pages/BookDetails';
+import Books from '@/pages/Books';
 import Home from '@/pages/Home';
+import Login from '@/pages/Login';
+import NewBook from '@/pages/NewBook';
+import Signup from '@/pages/SignUp';
 import { createBrowserRouter } from 'react-router-dom';
+import PrivetRoute from './PrivetRoute';
 
 const routes = createBrowserRouter([
   {
@@ -10,6 +17,35 @@ const routes = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: '/books',
+        element: <Books />,
+      },
+      {
+        path: '/books/book-details/:id',
+        element: <BookDetails />,
+      },
+      {
+        path: '/books/:id',
+        element: <EditBook />,
+      },
+      {
+        path: '/add-new',
+
+        element: (
+          <PrivetRoute>
+            <NewBook />,
+          </PrivetRoute>
+        ),
+      },
+      {
+        path: '/sign-up',
+        element: <Signup />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
       },
     ],
   },
