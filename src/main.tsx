44ef8fdb6181400085bf156064ce12b34d1358fc
sazzad <sazzadkarim01@gmail.com>
2023-07-19 20:24:45 +0600

    main.tsx file commited

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import routes from './routes/routes';
+import store from './redux/store.ts';
+import routes from './routes/routes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
   </React.StrictMode>
 );
