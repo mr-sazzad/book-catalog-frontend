75c71d36bd386cf71c36fccbfa6af3d0319d7dcf
sazzad <sazzadkarim01@gmail.com>
2023-07-19 20:41:38 +0600

    Footer modifide

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content mt-10">
       <div>
         <p>Copyright © 2023 - All right reserved by Book Catalog Ltd</p>
       </div>
