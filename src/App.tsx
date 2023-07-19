052ffe659ad8b7a35ad0e4fd1c9a8de14a64409d
sazzad <sazzadkarim01@gmail.com>
2023-07-19 20:23:07 +0600

    App.tsx commited

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { auth } from './Firebase/firebase';
 import Navbar from './components/Navbar';
+import { useAppDispatch } from './redux/hooks';
+import { setLoading, setUser } from './redux/users/userSlice';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(setLoading(true));
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser(user.email as string));
+
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="view-port">
       <Navbar />
