496839e98cc16ce9185d2d8eba780602969b5196
mr-sazzad <main.sazza@gmail.com>
2023-10-02 18:13:29 +0600

    book catalog baclend project modified

--- src/components/EditBook.tsx
+++ src/components/EditBook.tsx
@@ -127,7 +127,7 @@ function EditBook() {
         type="submit"
         className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-full"
       >
-        Book Update
+        Update The Book
       </button>
     </form>
   );
